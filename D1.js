739d030026f5179db2dc2105aa74700cd90bcb0a
andrea-maiolo <maiolo.m.2@gmail.com>
2025-03-17 16:45:28 +0100

    esercizio finito

--- D1.js
+++ D1.js
@@ -12,24 +12,41 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+// Le variabili  in JS sono come dei contenitori e i datatype sono il tipo di informazioni contenute nel contenitore.
 
+// I datatype possono essere numeri come: 2,56,-4, o anche 3,14 infatti JS ammette come numeri sia i numeri interi che quelli frazionati.
+
+// Possono essere string quindi parole o frasi all' interno di virgolette come: "Andrea","ciao
+// questa e una stringa".
+
+// Possono essere boolean quindi: vero (true) o falso (false)
+
+// Una variabile puo essere anche undefined se per esempio definisco una variabile ma non assegno nessun valore: let myVar
+
+// Una variabile puo essere null che e simile a undefined ma qui sono io che ho deciso che questa variabile dovrebbe essere vuota.
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let myName = "Andrea";
+console.log(myName);
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let sum = 12 + 20;
+console.log(sum);
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12;
+console.log(x);
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
@@ -37,12 +54,25 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+myName = "Maiolo";
+console.log(myName);
+
+const varInvariable = 3;
+console.log(varInvariable);
+
+// CONSTANTE NON CAMBIABILE
+// varInvariable = 5;
+// console.log(varInvariable);
+// D1.js:51 Uncaught TypeError: Assignment to constant variable.
+//     at D1.js:51:15
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let subtraction = 4 - x;
+console.log(subtraction);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -51,3 +81,9 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let name1 = "john";
+let name2 = "John";
+
+console.log("le variabili sono diverse", name1 != name2);
+
+console.log("le variabili sono uguali se trasformate", name1 === name2.toLowerCase());
